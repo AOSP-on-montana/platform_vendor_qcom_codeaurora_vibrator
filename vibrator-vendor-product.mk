1f9fa608b448cc38e44aeb75d8e4a68a55188456
Jarl-Penguin <jarlpenguin@outlook.com>
2021-10-07 08:59:57 +0300

    Update paths
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -12,5 +12,5 @@ ifneq ($(BOARD_OPENSOURCE_DIR), )
       $(BOARD_OPENSOURCE_DIR)/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 else
       PRODUCT_COPY_FILES += \
-      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
+      vendor/qcom/codeaurora/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 endif # BOARD_OPENSOURCE_DIR
